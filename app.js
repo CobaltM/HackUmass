bba5bd9b3de9f10ff2e00dc7ef31672bd782f9c5
Brian <brhaufler@vassar.edu>
2018-10-14 05:27:40 -0400

    Removed hard-coded PORT from app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/room/script.js',function(req,res){
 
 /* Server Configuration Section */
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT, function () {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Example app listening at http://%s:%s", host, port)
